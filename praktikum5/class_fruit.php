248f8e105be446717d89f4fa6be9d3648a5fbc89
anafath10 <92621896+anafath10@users.noreply.github.com>
2022-04-23 20:41:48 +0700

    Add files via upload

---
+++ praktikum5/class_fruit.php
@@ -0,0 +1,16 @@
+<?php
+
+class Fruit{
+
+    public $name;
+    protected $color;
+    private $weight;
+
+}
+
+$mango = new Fruit();
+$mango->name = 'Mango';
+$mango->color = 'Yellow';
+$mango->weight = '300';
+
+?>
